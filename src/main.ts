e156dc9360adc15cfce20f0cff1bfdccf89534f3
Bantamlak <neoluka315@gmail.com>
2024-07-23 10:31:24 +0300

    the main app definition

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import * as passport from 'passport';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuring session middleware
+  app.use(
+    session({
+      secret: process.env.SESSION_KEY,
+      resave: false,
+      saveUninitialized: false,
+      cookie: { secure: process.env.NODE_ENV === 'production' ? true : false },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  // Set Global validation pipeline
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
