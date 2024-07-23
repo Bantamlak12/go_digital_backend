0c55584099c96a2611cf2cac233a503368d64b34
Bantamlak <neoluka315@gmail.com>
2024-07-23 10:28:23 +0300

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminModule } from './admin/admin.module';
@@ -9,9 +11,39 @@ import { ContactModule } from './contact/contact.module';
 import { ServicesModule } from './services/services.module';
 import { TestimonialsModule } from './testimonials/testimonials.module';
 import { VacanciesModule } from './vacancies/vacancies.module';
+import { Admin } from './auth/auth.entity';
 
 @Module({
-  imports: [AdminModule, AuthModule, BlogModule, BookingModule, ContactModule, ServicesModule, TestimonialsModule, VacanciesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get<string>('DATABASE_HOST'),
+        port: parseInt(config.get<string>('DATABASE_PORT')),
+        username: config.get<string>('DATABASE_USERNAME'),
+        password: config.get<string>('DATABASE_PASSWORD'),
+        database: config.get<string>('DATABASE_NAME'),
+        entities: [Admin],
+        synchronize: process.env.NODE_ENV !== 'development' ? false : true,
+        ssl:
+          config.get<string>('NODE_ENV') !== 'development'
+            ? { rejectUnauthorized: false }
+            : false,
+      }),
+    }),
+    AdminModule,
+    AuthModule,
+    BlogModule,
+    BookingModule,
+    ContactModule,
+    ServicesModule,
+    TestimonialsModule,
+    VacanciesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
