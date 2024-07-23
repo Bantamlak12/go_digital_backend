c21e44df8d2cfdbed4d84567eb9f8b02b258d864
Bantamlak <neoluka315@gmail.com>
2024-07-23 10:23:33 +0300

    admin handled database creation and imported AuthModule

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { Admin } from '../auth/auth.entity';
 
 @Module({
+  // This is the line that create the Repository
+  imports: [TypeOrmModule.forFeature([Admin]), forwardRef(() => AuthModule)],
   controllers: [AdminController],
-  providers: [AdminService]
+  providers: [AdminService],
+  exports: [AdminService],
 })
 export class AdminModule {}
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Admin } from '../auth/auth.entity';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(@InjectRepository(Admin) private repo: Repository<Admin>) {}
+
+  create(email: string, password: string): Promise<Admin> {
+    const admin = this.repo.create({ email, password });
+
+    return this.repo.save(admin);
+  }
+
+  async find(email: string): Promise<Admin[]> {
+    return await this.repo.find({ where: { email } });
+  }
+}
